d602c0d6006e55f7d0f0be4dc9e97f985bdc5874
Matia <matia.casu@gmail.com>
2021-03-03 17:06:29 +0100

    Sistemati controlli Input, aggiunta stampa posizione Utente

--- js/script.js
+++ js/script.js
@@ -1,19 +1,26 @@
-/*
-Chiedere cognome utente
-inserirlo in array con altri cognomi.
-Stampare la lista ordinata alfabeticamente
-scrivere posizione "umana" utente.
-*/
-
 // Lista a cui aggiungere input utente
-var cognLista = ["Brambilla", "Fumagalli", "Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+var cognLista = ["brambilla", "fumagalli", "bianchi", "rossi", "duzioni", "balsano", "verdi"];
 
 // Cognome Utente
-var cognome = (prompt("Inserisci cognome"));
-cognLista.push(cognome);
+var cognome = prompt("Inserisci cognome");
+if(isNaN(cognome)){
+  cognome = cognome.toLowerCase();
+  cognLista.push(cognome);
+}
+else{
+  alert("Hai inserito un numero!")
+}
 // /Cognome Utente
 
 // Lista in ordine alfabetico
-cognLista.sort();
-console.log(cognLista);
+var listaOrdinata = cognLista.sort();
 // /Lista in ordine alfabetico
+
+// Stampa Posizione Utente
+for(var i = 0; i < cognLista.length; i++){
+  if(cognome === listaOrdinata[i]){
+    listaOrdinata[i] = cognome + " N': " + (i + 1);
+  }
+  console.log(listaOrdinata[i]);
+}
+// /Stampa Posizione Utente
